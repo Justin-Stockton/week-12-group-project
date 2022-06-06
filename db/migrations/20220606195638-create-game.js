bf148d83ca544fa64a89bc74cd60fb97bd8c3930
allan <ayang740@gmail.com>
2022-06-06 15:59:28 -0400

    game models

---
+++ db/migrations/20220606195638-create-game.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        unique: true
+      },
+      tags: {
+        allowNull: false,
+        type: Sequelize.STRING(15)
+      },
+      gameImage: {
+        allowNull: false,
+        type: Sequelize.STRING(150)
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Games');
+  }
+};
