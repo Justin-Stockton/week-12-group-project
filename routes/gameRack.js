4932e24a7a1c5cec34a83cb8dd35429fdc639d73
Justin-Stockton <justinstockton67@gmail.com>
2022-06-08 17:20:51 -0400

    possible working route

--- routes/gameRack.js
+++ routes/gameRack.js
@@ -7,7 +7,7 @@ const { csrfProtection, asyncHandler } = require("./utils");
 const db = require("../db/models");
 const { requireAuth, restoreUser } = require("../auth");
 
-const { User, PlayingGame, Game } = db;
+const { User, PlayingGame, Game, Rack } = db;
 
 //==== get playing rack for a user ====//
 
@@ -21,10 +21,26 @@ router.get(
     //==== where the userId is the same as the logged in user ====//
     const gamesRacks = await PlayingGame.findAll({
       where: { userId: req.session.auth.userId },
-      include: [{ model: Game }]
+      include: [{ model: Game }],
     });
     // console.log(gamesRacks[0].Game)
     res.render("gamesRack", { title: "Games Racks", gamesRacks });
+  })
+);
+
+router.post(
+  "/add",
+  csrfProtection,
+  restoreUser,
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const { rackName } = req.body;
+    const newRack = Rack.build({
+      name: rackName,
+      userId: req.session.auth.userId,
+      csrfToken: req.csrfToken(),
+    });
+    res.render("/");
   })
 );
 
