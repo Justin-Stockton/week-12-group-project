5dfaf4749b12b7a110ac536b8052af9593f48264
allan <ayang740@gmail.com>
2022-06-08 15:46:31 -0400

    show username

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { asyncHandler } = require("./utils");
+const { csrfProtection, asyncHandler } = require("./utils");
 const { requireAuth, restoreUser } = require("../auth");
+const db = require("../db/models");
+const { User } = db;
+
 
 // THIS IS THE USER HOME PAGE - USER PROFILE PAGE
 
-router.get("/", requireAuth, restoreUser, (req, res) => {
+router.get("/", requireAuth, restoreUser, csrfProtection, asyncHandler (async (req, res) => {
+  const users = await User.findAll({ order: [["username", "DESC"]] });
   res.render("home", {
     title: "Home",
+    users,
+    csrfToken: req.csrfToken(),
   });
-});
+}));
 
 module.exports = router;
