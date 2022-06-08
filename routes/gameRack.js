9b3f2a711612a9af86092480a909a3599c397532
Justin-Stockton <justinstockton67@gmail.com>
2022-06-08 17:40:58 -0400

    realized that should be in home not game-racks

--- routes/gameRack.js
+++ routes/gameRack.js
@@ -28,42 +28,4 @@ router.get(
   })
 );
 
-/* ==== with a text field and a submit button this should make a
-        this route "should" add a game rack with the name they enter
-        and tie that to their user id ==== */
-
-router.post(
-  "/add",
-  csrfProtection,
-  restoreUser,
-  requireAuth,
-  asyncHandler(async (req, res, next) => {
-    const { rackName } = req.body;
-    const newRack = Rack.build({
-      name: rackName,
-      userId: req.session.auth.userId,
-      csrfToken: req.csrfToken(),
-    });
-    res.render("/");
-  })
-);
-
-/* ==== This should delete all games racks created where the user will
-        have to enter the name of the rack they want to delete ==== */
-
-router.post(
-  "/delete",
-  csrfProtection,
-  restoreUser,
-  requireAuth,
-  asyncHandler(async (req, res, next) => {
-    const userId = req.session.auth.userId;
-    const { rackName } = req.body;
-    await Rack.destroy({
-      where: { name: rackName, userId },
-    });
-    res.render("/");
-  })
-);
-
 module.exports = router;
