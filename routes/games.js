750dba4aa0e28374e65d3bfd33ecf403efa1d2a3
Justin-Stockton <justinstockton67@gmail.com>
2022-06-07 22:44:30 -0400

    working git route for games by id

--- routes/games.js
+++ routes/games.js
@@ -4,22 +4,40 @@ const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("./utils");
 const router = express.Router();
 const { userValidators, loginValidators } = require("../validations");
-const { Game } = db;
-
-router.get("/", csrfProtection, (req, res) => {
-  res.render("games", { csrfToken: req.csrfToken() });
-});
+const { Game, User, PlayingGame, Review } = db;
 
 router.get(
-  "/:id",
+  "/",
   asyncHandler(async (req, res) => {
-    // const gameIdx = req.params.gameId
-    // console.log(gameIdx)
-
-    const game = await Game.findAll();
+    const game = await Game.findAll({ order: [["name", "ASC"]] });
     console.log(game);
-    res.send({ message: "work" });
-    res.render("gamesId", { csrfToken: req.csrfToken(), game });
+    res.render("games", {
+      title: "Games",
+      game,
+    });
+  })
+);
+
+router.get(
+  "/:gameId(\\d+)",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const gameId = parseInt(req.params.gameId, 10);
+    console.log(gameId);
+    const game = await Game.findByPk(gameId);
+    const user = await User.findByPk();
+    const reviews = await Review.findAll({
+      where: {
+        gameId,
+      },
+    });
+
+    if (!game) {
+      res.redirect("/404");
+    }
+    const auth = req.session.auth;
+
+    res.render("gamesId", { game, user, reviews, auth });
   })
 );
 
