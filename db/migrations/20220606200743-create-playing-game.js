9c0287c28cd46a3f3a009747f6dd65127555b1dd
Justin-Stockton <justinstockton67@gmail.com>
2022-06-09 11:49:01 -0400

    DB round 3.2

--- db/migrations/20220606200743-create-playing-game.js
+++ db/migrations/20220606200743-create-playing-game.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('PlayingGames', {
+    return queryInterface.createTable("PlayingGames", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: "Users" }
+        references: { model: "Users" },
       },
       gameId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: "Games" }
+        references: { model: "Games" },
       },
-      timePlayed: {
-        allowNull: false,
-        type: Sequelize.INTEGER
+      rackId: {
+        type: Sequelize.INTEGER,
+        references: { model: "Racks" },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('PlayingGames');
-  }
+    return queryInterface.dropTable("PlayingGames");
+  },
 };
