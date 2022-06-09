851cc2903815a0a597f65fd8e7e686049a944b8a
Justin-Stockton <justinstockton67@gmail.com>
2022-06-09 11:56:28 -0400

    removed ownerships

--- db/models/game.js
+++ db/models/game.js
@@ -1,17 +1,24 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Game = sequelize.define('Game', {
-    name: DataTypes.STRING,
-    tags: DataTypes.STRING,
-    gameImage: DataTypes.STRING,
-    description: DataTypes.TEXT
-  }, {});
-  Game.associate = function(models) {
-    Game.hasMany(models.Ownership, {foreignKey: "gameId"});
-    Game.hasMany(models.Review, {foreignKey: "gameId"});
-    Game.hasMany(models.PlayingGame, {foreignKey: "gameId"})
-    Game.belongsToMany(models.User, {through:"Ownership", foreignKey: "gameId", otherKey: "userId"});
-    Game.hasMany(models.Rack, {foreignKey: "gameId"});
+  const Game = sequelize.define(
+    "Game",
+    {
+      name: DataTypes.STRING,
+      tags: DataTypes.STRING,
+      gameImage: DataTypes.STRING,
+      description: DataTypes.TEXT,
+    },
+    {}
+  );
+  Game.associate = function (models) {
+    Game.hasMany(models.Review, { foreignKey: "gameId" });
+    Game.hasMany(models.PlayingGame, { foreignKey: "gameId" });
+    Game.belongsToMany(models.User, {
+      through: "Ownership",
+      foreignKey: "gameId",
+      otherKey: "userId",
+    });
+    Game.hasMany(models.Rack, { foreignKey: "gameId" });
   };
   return Game;
 };
